e73e2a680ada5d7d7fc9b8d2f2857c6f27a5ec3a
Richard Samuelson <noobtoothfairy@gmail.com>
2017-10-04 05:13:24 +0200

    refined usage of the mixed typed value of the baseAttr object by explicitly wrtiting the expressions equivalent to falsy checking baseAttr.value

--- src/util/attributesComparator.js
+++ src/util/attributesComparator.js
@@ -2,41 +2,39 @@
  * @flow
  */
 
-import {
-  getLiteralPropValue,
-  propName,
-} from 'jsx-ast-utils';
-import type { Node } from 'ast-types-flow';
+import { getLiteralPropValue, propName } from "jsx-ast-utils";
+import type { Node } from "ast-types-flow";
 
 /**
  * Returns true if all items in baseAttributes are found in attributes. Always
  * returns true if baseAttributes is empty.
  */
 function attributesComparator(
-  baseAttributes: Array<{[key: string]: mixed}> = [],
-  attributes: Array<Node> = [],
+  baseAttributes: Array<{ [key: string]: mixed }> = [],
+  attributes: Array<Node> = []
 ): boolean {
-  return baseAttributes.every(
-    (baseAttr): boolean => attributes.some(
-      (attribute): boolean => {
-        // Guard against non-JSXAttribute nodes like JSXSpreadAttribute
-        if (attribute.type !== 'JSXAttribute') {
-          return false;
-        }
-        // Attribute matches.
-        if (baseAttr.name !== propName(attribute)) {
-          return false;
-        }
-        // Value exists and does not match.
-        if (
-          baseAttr.value
-          && baseAttr.value !== getLiteralPropValue(attribute)
-        ) {
-          return false;
-        }
-        return true;
-      },
-    ),
+  return baseAttributes.every((baseAttr): boolean =>
+    attributes.some((attribute): boolean => {
+      // Guard against non-JSXAttribute nodes like JSXSpreadAttribute
+      if (attribute.type !== "JSXAttribute") {
+        return false;
+      }
+      // Attribute matches.
+      if (baseAttr.name !== propName(attribute)) {
+        return false;
+      }
+      // Value exists and does not match.
+      if (
+        baseAttr.value !== null &&
+        baseAttr.value !== false &&
+        baseAttr.value !== undefined &&
+        Object.prototype.hasOwnProperty.call(baseAttr, "value") &&
+        baseAttr.value !== getLiteralPropValue(attribute)
+      ) {
+        return false;
+      }
+      return true;
+    })
   );
 }
 
